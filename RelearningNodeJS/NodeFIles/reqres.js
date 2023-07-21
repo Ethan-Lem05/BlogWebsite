df72221fe769cb819b422620499643c2b1823c3a
Ethan-Lem05 <skullerboy88@gmail.com>
2023-07-21 15:33:14 -0400

    Update reqres.js

--- RelearningNodeJS/NodeFIles/reqres.js
+++ RelearningNodeJS/NodeFIles/reqres.js
@@ -3,28 +3,29 @@ const fs  = require("fs");
 
 const server = http.createServer((req,res) => {
     //variables 
-    let path = "./views";
+    let path = "";
     //request properties ex req.url req.method (the type of request) etc.
     switch(req.url) {
         case "/": 
-            path += home;
-        case "404":
-            path += 404;
+            path += "/home.html";
+        case "/404":
+            path += "404.html";
             break;
-        case "about":
-            path += about;
+        case "/about":
+            path += "/about.html";
             break;
+        
     }
     //response properties
     res.setHeader("Content-Type", "text/html") // text/plain returns plain text, text/html returns html 
-    fs.readFile("./views/Home.html", (err,data) => {
+    fs.readFile("./RelearningNodeJS/views" + path, (err,data) => {
         if(err) {
             console.log(err);
             res.end();
             return;
         }
-        res.write(data);
-        res.end();
+        res.write(data); //writes data to the response object
+        res.end();//sends the response 
     });
 });
 
