2cca34a96d0a2e31124079b2839205c5b8040f24
Ethan-Lem05 <skullerboy88@gmail.com>
2023-07-21 12:18:14 -0400

    Added node terminal npm package thingy
    added basic routing as well

--- Relearning NodeJS/NodeFIles/reqres.js
+++ Relearning NodeJS/NodeFIles/reqres.js
@@ -2,16 +2,28 @@ const http = require("http");
 const fs  = require("fs");
 
 const server = http.createServer((req,res) => {
-    //request properties
-    console.log(req.url, req.method);
+    //variables 
+    let path = "./views";
+    //request properties ex req.url req.method (the type of request) etc.
+    switch(req.url) {
+        case "/": 
+            path += home;
+        case "404":
+            path += 404;
+            break;
+        case "about":
+            path += about;
+            break;
+    }
     //response properties
     res.setHeader("Content-Type", "text/html") // text/plain returns plain text, text/html returns html 
     fs.readFile("./views/Home.html", (err,data) => {
         if(err) {
             console.log(err);
-        } else {
-            res.write(data);
+            res.end();
+            return;
         }
+        res.write(data);
         res.end();
     });
 });
