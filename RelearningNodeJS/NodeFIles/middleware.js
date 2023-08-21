a766e4d1da214295bfdfa280d9f6b398c3982dce
Ethan-Lem05 <88153797+Ethan-Lem05@users.noreply.github.com>
2023-08-21 12:00:27 -0400

    installed morgon and added css

---
+++ RelearningNodeJS/NodeFIles/middleware.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "RelearningNodeJS/views");
+
+app.listen(3000, "localhost");
+
+//middleware is just code that runs between the request and the response
+
+/** 
+app.use((req,res, next) => {
+    console.log("new request has been made");
+    console.log("host: ", req.hostname);
+    console.log("path: ", req.path);
+    console.log("method: ", req.method); 
+    next();
+});
+app.use((req,res,next) => {
+    console.log("In the next middleware");
+    next();
+}) 
+*/
+
+//middleware
+app.use(morgan("dev")); // morgan is a 3rd party middleware that allows us to log our requests
+app.use(express.static("RelearningNodeJS/public"));
+
+app.get("/", (req,res) => {
+    //res.send("<p> home page </p>"); //writes and ends the response
+    //res.sendFile("/views/home.html", {root: "./RelearningNodeJS"}); // responds with a file and ends the response 
+
+    const blogs = [
+        {title: "First blog", snippet: "dshgsdljk hfkjdf"},
+        {title: "second blog", snippet: "kdaghskjdhfjkashd ksadjfhsd"},
+        {title: "third blog", snippet: "asdfahdk akdjfh sadkfjh"}
+    ];
+
+    res.render("index", {title: "home", blogs: blogs}); // renders a view for dynamic data 
+});
+
+app.get("/about", (req,res) => {
+    //res.send("<p> about page </p>");
+    res.render("about", { title: "about" });
+})
+
+app.get("/about-us", (req,res) => { // routing from about-us to about
+    res.redirect("/about");
+} ) 
+
+app.get("/create", (req,res) => {
+    res.render("create.ejs", { title: "create" })
+});
+
+
+//handles 404 if none of the above get handlers are called -> middleware  
+app.use((req,res) => {
+    //res.status(404).sendFile("/views/404.html", {root: "./RelearningNodeJS"}); // status function returns the response object and changes status
+    res.status(404).render("404");
+});
+
+//get handlers only get called 
